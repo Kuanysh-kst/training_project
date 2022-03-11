6c942ea0ba7d8d48feaef331f42bfd368e7c39fb
Kuanysh-kst <90975457+Kuanysh-kst@users.noreply.github.com>
2022-03-11 19:19:26 +0600

    создал кнопку

--- lib/screens/timer_screen.dart
+++ lib/screens/timer_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/button_widget.dart';
 
 class TimerScreen extends StatefulWidget {
   TimerScreen({Key? key}) : super(key: key);
@@ -10,17 +11,27 @@ class TimerScreen extends StatefulWidget {
 class _TimerScreenState extends State<TimerScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.blue,
-        title: Center(child: Text('Timer Screen'))),
-      body: Container(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [],
-      )),
-    ));
+    {
+      {
+        buildButtons() {
+          return Center(child: ButtonWidget(text: 'Start Timer!', Oncklicked: () {}));
+        }
+
+        return SafeArea(
+            child: Scaffold(
+          appBar: AppBar(
+              elevation: 0,
+              backgroundColor: Colors.blue,
+              title: Center(child: Text('Timer Screen'))),
+          body: Container(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              buildButtons(),
+            ],
+          )),
+        ));
+      }
+    }
   }
 }
